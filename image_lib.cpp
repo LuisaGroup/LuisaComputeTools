032004a0cf215c6f5a733e3d7501e7ef5324b0e2
maxwellgeng@outlook.com <maxwellgeng@outlook.com>
2022-11-12 13:40:46 +0800

    load & save image

--- image_lib.cpp
+++ image_lib.cpp
@@ -2,20 +2,79 @@
 #include <vstl/common.h>
 #include <core/binary_io_visitor.h>
 namespace luisa::compute {
-namespace imglib_detail {
-class ImageHeader {
-    uint32_t width, height, volume, mip_level;
-    PixelStorage storage;   
-};
-}// namespace imglib_detail
-Image<float> ImageLib::load_float_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer) {
-    return {};
+size_t img_byte_size(PixelStorage storage, uint width, uint height, uint volume, uint mip_level) {
+    size_t size = 0;
+    for (auto i : vstd::range(mip_level)) {
+        size += pixel_storage_size(storage, width, height, volume);
+        width /= 2;
+        height /= 2;
+    }
+    return size;
 }
-Image<int> ImageLib::load_int_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer) {
-    return {};
 
+namespace imglib_detail {
+struct ImageHeader {
+    uint width, height, mip_level, volume;
+    PixelStorage storage;
+};
+template<typename T>
+Image<T> load_image_impl(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    ImageHeader header;
+    bin_stream->read({reinterpret_cast<std::byte *>(&header), sizeof(ImageHeader)});
+    auto byte_size = img_byte_size(header.storage, header.width, header.height, 1, header.mip_level);
+    vstd::vector<std::byte> data;
+    data.push_back_uninitialized(byte_size);
+    bin_stream->read(data);
+    auto img = device.create_image<T>(header.storage, header.width, header.height, header.mip_level);
+    cmd_buffer << img.copy_from(data.data()) << [data = std::move(data)] {};
+    return img;
 }
-Image<uint32_t> ImageLib::load_uint_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer) {
-    return {};
+template<typename T>
+Volume<T> load_volume_impl(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    ImageHeader header;
+    bin_stream->read({reinterpret_cast<std::byte *>(&header), sizeof(ImageHeader)});
+    auto byte_size = img_byte_size(header.storage, header.width, header.height, header.volume, header.mip_level);
+    vstd::vector<std::byte> data;
+    data.push_back_uninitialized(byte_size);
+    bin_stream->read(data);
+    auto img = device.create_volume<T>(header.storage, header.width, header.height, header.mip_level);
+    cmd_buffer << img.copy_from(data.data()) << [data = std::move(data)] {};
+    return img;
+}
+
+}// namespace imglib_detail
+Image<float> ImageLib::load_float_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_image_impl<float>(bin_stream, device, cmd_buffer);
+}
+Image<int> ImageLib::load_int_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_image_impl<int>(bin_stream, device, cmd_buffer);
+}
+Image<uint> ImageLib::load_uint_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_image_impl<uint>(bin_stream, device, cmd_buffer);
+}
+Volume<float> ImageLib::load_float_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_volume_impl<float>(bin_stream, device, cmd_buffer);
+}
+Volume<int> ImageLib::load_int_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_volume_impl<int>(bin_stream, device, cmd_buffer);
+}
+Volume<uint> ImageLib::load_uint_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+    return imglib_detail::load_volume_impl<uint>(bin_stream, device, cmd_buffer);
+}
+size_t ImageLib::header_size() {
+    return sizeof(imglib_detail::ImageHeader);
+}
+void ImageLib::save_header(
+    luisa::span<std::byte> data,
+    uint width, uint height, uint volume,
+    PixelStorage storage,
+    uint mip) {
+    imglib_detail::ImageHeader header{
+        .width = width,
+        .height = height,
+        .mip_level = mip,
+        .volume = volume,
+        .storage = storage};
+    memcpy(data.data(), &header, sizeof(imglib_detail::ImageHeader));
 }
 }// namespace luisa::compute
--- image_lib.h
+++ image_lib.h
@@ -1,26 +1,69 @@
 #pragma once
 #include <tools/config.h>
 #include <runtime/image.h>
+#include <runtime/volume.h>
 #include <runtime/command_buffer.h>
 namespace luisa::compute {
 class IBinaryStream;
 class LC_TOOL_API ImageLib {
+public:
+    using WriteFunc = luisa::move_only_function<void(luisa::span<std::byte const> data)>;
+
+private:
     ImageLib() = delete;
-    static Image<float> load_float_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer);
-    static Image<int> load_int_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer);
-    static Image<uint32_t> load_uint_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer);
+    static Image<float> load_float_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static Image<int> load_int_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static Image<uint32_t> load_uint_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static Volume<float> load_float_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static Volume<int> load_int_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static Volume<uint32_t> load_uint_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer);
+    static size_t header_size();
+    static void save_header(
+        luisa::span<std::byte> data,
+        uint width, uint height, uint volume,
+        PixelStorage storage,
+        uint mip);
 
 public:
     template<typename T>
         requires(is_legal_image_element<T>)
-    static Image<T> load_image(IBinaryStream *io_visitor, Device &device, CommandBuffer &cmd_buffer) {
+    static Image<T> load_image(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
         if constexpr (std::is_same_v<T, float>) {
-            return load_float_image(io_visitor, device, cmd_buffer);
+            return load_float_image(bin_stream, device, cmd_buffer);
         } else if constexpr (std::is_same_v<T, int32_t>) {
-            return load_int_image(io_visitor, device, cmd_buffer);
+            return load_int_image(bin_stream, device, cmd_buffer);
         } else {
-            return load_uint_image(io_visitor, device, cmd_buffer);
+            return load_uint_image(bin_stream, device, cmd_buffer);
         }
+    }
+    template<typename T>
+        requires(is_legal_image_element<T>)
+    static Volume<T> load_volume(IBinaryStream *bin_stream, Device &device, CommandBuffer &cmd_buffer) {
+        if constexpr (std::is_same_v<T, float>) {
+            return load_float_image(bin_stream, device, cmd_buffer);
+        } else if constexpr (std::is_same_v<T, int32_t>) {
+            return load_int_image(bin_stream, device, cmd_buffer);
+        } else {
+            return load_uint_image(bin_stream, device, cmd_buffer);
+        }
+    }
+    template<typename T>
+    static void save_image(Image<T> const &image, CommandBuffer &cmd_buffer, WriteFunc &&func) {
+        luisa::vector<std::byte> bytes;
+        auto header = header_size();
+        bytes.push_back_uninitialized(header + image.byte_size());
+        cmd_buffer << image.copy_to(bytes.data() + header) << [func = std::move(func), bytes = std::move(bytes), size = image.size(), storage = image.storage(), mip = image.mip_levels()] {
+            save_header(bytes, size.x, size.y, 1, storage, mip);
+        };
+    }
+    template<typename T>
+    static void save_volume(Volume<T> const &image, CommandBuffer &cmd_buffer, WriteFunc &&func) {
+        luisa::vector<std::byte> bytes;
+        auto header = header_size();
+        bytes.push_back_uninitialized(header + image.byte_size());
+        cmd_buffer << image.copy_to(bytes.data() + header) << [func = std::move(func), bytes = std::move(bytes), size = image.size(), storage = image.storage(), mip = image.mip_levels()] {
+            save_header(bytes, size.x, size.y, size.z, storage, mip);
+        };
     }
 };
 }// namespace luisa::compute
